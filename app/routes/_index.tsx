1ba98224194dc89ae922e867eb8d419a8afabfb7
Erik Faz <43554741+shadowfaz16@users.noreply.github.com>
2025-01-26 20:06:03 -0500

    deploy?

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -11,7 +11,7 @@ export const meta: MetaFunction = () => {
     { name: 'description', content: 'Talk with Collective Coder, an AI assistant from The Collective' },
   ];
 };
-
+ 
 export const loader = () => json({});
 
 export default function Index() {
